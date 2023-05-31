286398747c1b5c8754025d9e0b4439c3c18dd958
v1centebarros <vmabarros@ua.pt>
2023-05-31 17:50:54 +0100

    Update Dockerfiles

--- src/pages/error404/error404.tsx
+++ src/pages/error404/error404.tsx
@@ -1,8 +1,6 @@
-import styles from "./error404.module.css";
-
 interface Error404Props {}
 
 export const Error404: React.FunctionComponent<Error404Props> = (props) => {
-  return <div className={styles.error404}>
+  return <div>
     404</div>;
 };
---
+++ src/pages/statistics/statistics.tsx
@@ -0,0 +1,10 @@
+import styles from "./statistics.module.css";
+import {Container} from "@/components/container";
+
+interface StatisticsProps {}
+
+export const Statistics: React.FunctionComponent<StatisticsProps> = (props) => {
+  return <Container>
+
+  </Container>
+};
