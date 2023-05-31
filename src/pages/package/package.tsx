01d0ce8a3b3db233b48191013839773550190c6e
v1centebarros <vmabarros@ua.pt>
2023-05-31 01:04:34 +0100

    RR-8 Add and Remove Pickup Page RR-16 Search for Nearby PickUps RR-10

---
+++ src/pages/package/package.tsx
@@ -0,0 +1,90 @@
+import {Container} from "@/components/container";
+import {Loader} from "@/components/loader";
+import {Card, Timeline} from "flowbite-react";
+import {useQuery} from "react-query";
+import {Package as PackageType} from "@/types";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {URLS} from "@/urls";
+import moment from "moment";
+
+interface PackageProps {
+}
+
+export const Package: React.FunctionComponent<PackageProps> = (props) => {
+    const params = useParams()
+    const {data, status} = useQuery<PackageType>({
+        queryKey: ['package', params.id],
+        queryFn: async () => {
+            const {data} = await axios.get(URLS.packageDetails(params.id || ""));
+            return data;
+        }
+    });
+
+
+    return <Container>
+        <Loader status={status}>
+            <div className={"w-10/12 mt-5 space-y-4 mx-auto"}>
+                <Card>
+                    <div className={"grid grid-cols-12"}>
+                        <div className={"col-span-10"}>
+                            <h2 className={"text-3xl font-bold"}>User</h2>
+                            <div className={"text-2xl flex flex-row items-center space-x-1"}>
+                                {data?.customer.firstName} {data?.customer.lastName}
+                            </div>
+                            <div className={"text-xl font-extralight flex flex-row items-center space-x-1"}>
+                                {data?.customer.email}
+                            </div>
+
+                            <div className={"text-xl font-extralight flex flex-row items-center space-x-1"}>
+                                {data?.customer.phone}
+                            </div>
+
+                            <hr className={"mx-2 my-4"}/>
+
+                            <h2 className={"text-3xl font-bold"}>Shop</h2>
+                            <div className={"text-xl flex flex-row items-center space-x-1"}>
+                                {data?.shop.name}
+                            </div>
+                            <div className={"text-xl font-extralight flex flex-row items-center space-x-1"}>
+                                {data?.shop.address}
+                            </div>
+
+                            <hr className={"mx-2 my-4"}/>
+
+                            <h2 className={"text-3xl font-bold"}>Pick Up Point</h2>
+                            <div className={"text-xl flex flex-row items-center space-x-1"}>
+                                {data?.pickUpLocation.name}
+                            </div>
+                            <div className={"text-xl font-extralight flex flex-row items-center space-x-1"}>
+                                {data?.pickUpLocation.address}
+                            </div>
+
+                            <div className={"text-xl font-extralight flex flex-row items-center space-x-1"}>
+                                {data?.pickUpLocation.city} ({data?.pickUpLocation.latitude}, {data?.pickUpLocation.longitude})
+                            </div>
+
+                        </div>
+
+                        <Timeline className={"col-span-2"}>
+                            {data?.states.map((state) => <>
+                                <Timeline.Item>
+                                    <Timeline.Point/>
+                                    <Timeline.Content>
+                                        <Timeline.Title>
+                                            {state.state}
+                                        </Timeline.Title>
+                                        <Timeline.Time>
+                                            {moment(state.date).format('MMMM Do YYYY h:mm:ss a')}
+                                        </Timeline.Time>
+                                    </Timeline.Content>
+                                </Timeline.Item>
+                            </>)}
+                        </Timeline>
+
+                    </div>
+                </Card>
+            </div>
+        </Loader>
+    </Container>
+};
---
+++ src/pages/pickUpPoints/pickUpPoints.tsx
@@ -0,0 +1,103 @@
+import {Container} from "@/components/container";
+import React from "react";
+import {Button, Table, TextInput} from "flowbite-react";
+import {useQuery} from "react-query";
+import axios from "axios";
+import {URLS} from "@/urls";
+import {PickUpPoint} from "@/types";
+import {Loader} from "@/components/loader";
+import {CiSearch} from "react-icons/all";
+
+interface PickUpPointsProps {
+}
+
+
+export const PickUpPoints: React.FunctionComponent<PickUpPointsProps> = (props) => {
+
+    const {data, status, refetch} = useQuery<PickUpPoint[]>({
+        queryKey: ['pickup-points'],
+        queryFn: async () => {
+            const {data} = await axios.get(URLS.pickUp);
+            return data;
+        }
+    });
+    const [city, setCity] = React.useState<string>("");
+
+    const acceptPickUpPoint = async (id: string) => {
+        await axios.put(URLS.pickUpDetails(id), {})
+        await refetch();
+    }
+
+    const deletePickUpPoint = async (id: string) => {
+        await axios.delete(URLS.pickUpDetails(id))
+        await refetch();
+    }
+
+    return <Container>
+        <Loader status={status}>
+            <div className={"mx-auto w-10/12 mt-5 space-y-2"}>
+                <TextInput
+                    id="searchBar"
+                    placeholder="City"
+                    rightIcon={CiSearch}
+                    type="text"
+                    className={"w-2/12"}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+                <Table hoverable={true}>
+                    <Table.Head>
+
+                        <Table.HeadCell>
+                            ID
+                        </Table.HeadCell>
+                        <Table.HeadCell>
+                            Name
+                        </Table.HeadCell>
+                        <Table.HeadCell>
+                            Address
+                        </Table.HeadCell>
+                        <Table.HeadCell>
+                            City
+                        </Table.HeadCell>
+                        <Table.HeadCell colSpan={2} className={"text-center"}>
+                            Actions
+                        </Table.HeadCell>
+
+                    </Table.Head>
+                    <Table.Body className={"divide-y"}>
+                        {data && data.filter((pickUpPoint) => pickUpPoint.city.toLowerCase().includes(city.toLowerCase())).map((pickUpPoint) => <>
+                                <Table.Row key={pickUpPoint.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                    <Table.Cell>{pickUpPoint.id}</Table.Cell>
+                                    <Table.Cell>
+                                        {pickUpPoint.name}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {pickUpPoint.address}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {pickUpPoint.city} ({pickUpPoint.latitude}, {pickUpPoint.longitude})
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {pickUpPoint.accepted ? "Yes" : <Button
+                                            gradientMonochrome="success"
+                                            onClick={() => acceptPickUpPoint(pickUpPoint.id)}
+                                        >
+                                            <p>Accept</p>
+                                        </Button>}
+                                    </Table.Cell>
+                                    <Table.Cell className={"flex flex-row gap-2"}>
+                                        <Button
+                                            gradientMonochrome="failure"
+                                            onClick={() => deletePickUpPoint(pickUpPoint.id)}
+                                        ><p>Delete</p></Button>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </>
+                        )}
+                    </Table.Body>
+                </Table>
+            </div>
+        </Loader>
+
+    </Container>
+};
